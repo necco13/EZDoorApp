cc88f738cea5cd6e1fe345c0e347e1e0a2a23858
Nejc <nejcnarobe@gmail.com>
2024-01-23 21:58:14 +0100

    Added real login fixed post for tokens

--- EZDoorApp/MainPage.xaml.cs
+++ EZDoorApp/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace EZDoorApp;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+using Microsoft.Maui.Graphics;
+
+namespace EZDoorApp;
 
 public partial class MainPage : ContentPage
 {
@@ -10,7 +15,69 @@ public partial class MainPage : ContentPage
 
 	private async void OnLoginClicked(object sender, EventArgs e)
 	{
-        await Navigation.PushAsync(new UserPage());
+        TryLogin();
+    }
+
+    private async void TryLogin()
+    {
+        var requestBody = new
+        {
+            ime = UsernameEntry.Text,
+            geslo = PasswordEntry.Text
+        };
+
+        // Serialize the anonymous object to a JSON string
+        string jsonBody = JsonSerializer.Serialize(requestBody);
+
+        // Make a POST request with the JSON body
+        string resp = await MakePostRequest("https://ezdoor.azurewebsites.net/api/overi", jsonBody);
+        Console.WriteLine(resp);
+        if (resp.Equals("true"))
+        {
+            UsernameEntry.Text = "";
+            PasswordEntry.Text = "";
+            MessageLabel.Text = "";
+            await Navigation.PushAsync(new UserPage());
+        }
+        else
+        {
+            UsernameEntry.Text = "";
+            PasswordEntry.Text = "";
+            MessageLabel.Text = "Wrong username or password!";
+        }
+
+
+    }
+
+    private static async Task<string> MakePostRequest(string url, string jsonBody)
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Set the content type to JSON
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Create the HTTP content with the JSON body
+                HttpContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                // Make the POST request
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
     }
 }
 
--- EZDoorApp/UserPage.xaml.cs
+++ EZDoorApp/UserPage.xaml.cs
@@ -1,5 +1,7 @@
 using Org.Json;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Timers;
@@ -20,23 +22,57 @@ public partial class UserPage : ContentPage
     }
     private async void RefreshData(Object source, System.Timers.ElapsedEventArgs e)
     {
-        string token = await MakeGetRequest("https://ezdoor.azurewebsites.net/api/token");
-        int cifra  = JsonSerializer.Deserialize<Token>(token).cifra;
-        int cas = JsonSerializer.Deserialize<Token>(token).cas;
-        MainThread.BeginInvokeOnMainThread(() =>
+        // Create an anonymous object with the specified properties
+        var requestBody = new
         {
-            Code.Text = cifra.ToString();
-            Time.Text = cas.ToString();
-        });
+            ime = "nejcnarobe@gmail.com",
+            geslo = "!Geslo13"
+        };
+
+        // Serialize the anonymous object to a JSON string
+        string jsonBody = JsonSerializer.Serialize(requestBody);
+
+        // Make a POST request with the JSON body
+        string token = await MakePostRequest("https://ezdoor.azurewebsites.net/api/token", jsonBody);
+
+        // Deserialize the response and update UI
+        if (token != null)
+        {
+            string cleanedToken = token.Trim('"').Replace("\\", ""); ;
+
+            // Deserialize the cleaned response
+            Token tokenObject = JsonSerializer.Deserialize<Token>(cleanedToken);
+
+            int cifra = tokenObject.cifra;
+            int cas = tokenObject.cas;
+            if(cas == -1)
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopToRootAsync();
+                });
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Code.Text = cifra.ToString();
+                Time.Text = cas.ToString();
+            });
+        }
     }
 
-    private static async Task<string> MakeGetRequest(string url)
+    private static async Task<string> MakePostRequest(string url, string jsonBody)
     {
         try
         {
             using (HttpClient client = new HttpClient())
             {
-                HttpResponseMessage response = await client.GetAsync(url);
+                // Set the content type to JSON
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Create the HTTP content with the JSON body
+                HttpContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                // Make the POST request
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
                 if (response.IsSuccessStatusCode)
                 {
                     return await response.Content.ReadAsStringAsync();
